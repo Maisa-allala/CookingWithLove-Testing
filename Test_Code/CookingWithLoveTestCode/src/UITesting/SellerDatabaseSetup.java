f9dee114467ed3a7c988b16bdeec80cd4fca5c43
samarkal <159973080+samarkal@users.noreply.github.com>
2024-04-14 03:15:04 -0400

    Seller UI test and Database setup

---
+++ Test_Code/CookingWithLoveTestCode/src/UITesting/SellerDatabaseSetup.java
@@ -0,0 +1,119 @@
+package UITesting;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SellerDatabaseSetup {
+	// Define database connection parameters
+	String host = "localhost";
+	String port = "3306";
+	String database = "cooking_project";
+	String username = "root";
+	String password = "samar216898";
+
+	public void setupAdminData() throws SQLException {
+
+		// Establish database connection
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database,
+				username, password)) {
+			// Create a statement
+			try (Statement s = con.createStatement()) {
+
+				// Insert multiple records into the user_account table
+				int rowsAffected16 = s.executeUpdate(
+						"INSERT INTO user_account (accountID, userName, password, accountType, firstName, lastName, email, phone, street, zip, DOB, accountRequestedDate, accountActiveDate, is_active) "
+								+ "VALUES (200, 'admin2', '$2y$10$QsW/nP4iBattPEEfCxqJA.TXIXAxFUB/wWtYibMVWn8w6pVLuFDO2', 'Admin', 'Samar', 'Kal', 'Samar@yahoo.com', '415-567-8569', 'Alexis Road', '45678', '1997-10-05', '2023-12-01', '2023-12-11', 'Y')");
+
+				int rowsAffected20 = s.executeUpdate(
+						"INSERT INTO user_account (accountID, userName, password, accountType, firstName, lastName, email, phone, street, zip, DOB, accountRequestedDate, accountActiveDate, is_active) "
+								+ "VALUES (3, 'Samarkal', '$2y$10$qLhabk2IPx7ssCk3dd6GYeIBCcnJb2JWu/yxBic26up0grCmmNM2W', 'Seller', 'Samar', 'Kal', 'samar@yahoo.com', '415-567-8569', 'Alexis Road', '45678', '1997-10-05', '2023-12-23', '2023-12-11', 'Y')");
+
+				int rowsAffected3 = s.executeUpdate(
+						"INSERT INTO user_account (accountID, userName, password, accountType, firstName, lastName, email, phone, street, zip, DOB, accountRequestedDate, accountActiveDate, is_active) "
+								+ "VALUES (4, 'buyer1', '$2y$10$QsW/nP4iBattPEEfCxqJA.TXIXAxFUB/wWtYibMVWn8w6pVLuFDO2', 'Buyer', 'Tahleen', 'Tousef', 'maisa@yahoo.com', '415-567-8569', 'Alexis Road', '45678', '2024-01-02', '2023-12-11', null , 'Y')");
+
+				// Insert multiple records into the dishes table
+				int rowsAffected17 = s.executeUpdate(
+						"INSERT INTO dishes(dishID, dishName, description, price, status, dishType, accountID, publishDate, photoPath, is_published, DishRequestedDate) "
+								+ "VALUES (655, 'Banana Bread', 'Banana Bread', '4.00', 'available', 'desserts', 3, '2023-12-04', 'resources/banana-bread.jpg', 'Y', '2023-12-01')");
+				
+				int rowsAffected18 = s.executeUpdate(
+						"INSERT INTO dishes(dishID, dishName, description, price, status, dishType, accountID, publishDate, photoPath, is_published, DishRequestedDate) "
+								+ "VALUES (666, 'Chocolate Cupcake', 'Chocolate Cupcake', '4.00', 'available', 'desserts', 3, '2023-12-04', 'resources/chocolate-cupcake.jpg', 'Y', '2023-12-01')");
+				
+				int rowsAffected19 = s.executeUpdate(
+						"INSERT INTO dishes(dishID, dishName, description, price, status, dishType, accountID, publishDate, photoPath, is_published, DishRequestedDate) "
+								+ "VALUES (677, 'Banana Chocolatechip Bread', 'Banana Chocolatechip Bread', '4.00', 'available', 'desserts', 3, '2023-12-04', 'resources/Banana-Bread_square.jpg', 'Y', '2023-12-01')");
+				
+				// Insert into orderitems
+				int rowsAffected22 = s.executeUpdate("INSERT INTO orderitems (orderNum, DishID, DishName, quantity, price)" +
+	                    "VALUES ('9', '655', 'Banana Bread', '1', '4.00')");
+				
+				int rowsAffected25 = s.executeUpdate("INSERT INTO orderitems (orderNum, DishID, DishName, quantity, price)" +
+	                    "VALUES ('10', '666', 'Chocolate Cupcake', '1', '4.00')");
+				
+				int rowsAffected21 = s.executeUpdate("INSERT INTO orders (orderNum, date, deliveryType, totalPrice, cancelled, accountID, delivery_complete, order_complete) " +
+	                    "VALUES ('10', '2024-03-02', 'PickUp', '4.00', 'N', '4', 'N', 'N')");
+				
+				int rowsAffected23 = s.executeUpdate("INSERT INTO orders (orderNum, date, deliveryType, totalPrice, cancelled, accountID, delivery_complete, order_complete) " +
+	                     "VALUES ('9', '2024-03-02', 'PickUp', '4.00', 'N', '4', 'Y', 'Y')");
+			
+			
+				// Check if insertion was successful for both records
+				
+				if ( rowsAffected3 != 1|| rowsAffected16 != 1 || rowsAffected17 != 1 
+						 || rowsAffected22 != 1 || rowsAffected23 != 1 || rowsAffected21 != 1
+						|| rowsAffected18 != 1 || rowsAffected19 != 1 || rowsAffected20 != 1
+						|| rowsAffected25 != 1) {
+					throw new SQLException("Data insertion failed for one or more records.");
+				}
+			}
+		}
+	}
+
+	public void tearDownAdminData() throws SQLException {
+
+		// Establish database connection
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database,
+				username, password)) {
+			// Create a statement
+			try (Statement s = con.createStatement()) {
+
+			    // Disable foreign key checks to allow deletion
+			    s.executeUpdate("SET FOREIGN_KEY_CHECKS=0");
+			    
+				// Delete multiple records from the user_account and dishes table
+				int rowsAffected6 = s.executeUpdate("DELETE FROM dishes WHERE dishID = '" + 655 + "'");
+				int rowsAffected7 = s.executeUpdate("DELETE FROM dishes WHERE dishID = '" + 666 + "'");
+				int rowsAffected8 = s.executeUpdate("DELETE FROM dishes WHERE dishID = '" + 677 + "'");
+
+
+				int rowsAffected10 = s.executeUpdate("DELETE FROM orders WHERE orderNum = '" + 9 + "'");
+				int rowsAffected11 = s.executeUpdate("DELETE FROM orderitems WHERE orderNum = '" + 9 + "' AND dishID = '" + 655 + "'" );
+
+
+				int rowsAffected1 = s.executeUpdate("DELETE FROM user_account WHERE accountID = '" + 200 + "'");
+
+				int rowsAffected3 = s.executeUpdate("DELETE FROM user_account WHERE accountID = '" + 3 + "'");
+
+				int rowsAffected4 = s.executeUpdate("DELETE FROM user_account WHERE accountID = '" + 4 + "'");
+
+				
+			    // Enable foreign key checks
+			    s.executeUpdate("SET FOREIGN_KEY_CHECKS=1");
+
+
+				// Check if delete was successful for both records
+				if (rowsAffected1 != 1 || rowsAffected3 != 1 || rowsAffected4 != 1
+						|| rowsAffected6 != 1 || rowsAffected7 != 1 || rowsAffected8 != 1
+						|| rowsAffected10 != 1 || rowsAffected11 != 1 
+						) {
+					throw new SQLException("Failed to delete one or more records.");
+				}
+			}
+		}
+	}
+
+}
---
+++ Test_Code/CookingWithLoveTestCode/src/UITesting/SellerTest.java
@@ -0,0 +1,329 @@
+package UITesting;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
+public class SellerTest{
+	WebDriver driver;
+	SellerDatabaseSetup sellerDbSetup;
+
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		sellerDbSetup = new SellerDatabaseSetup(); 
+
+	}
+
+
+	@AfterClass
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+	
+	@BeforeClass
+	public void setUpAdminDatabase() {
+		try {
+
+			sellerDbSetup.setupAdminData();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@AfterClass
+	public void tearDownAdminDatabase() {
+		try {
+
+			sellerDbSetup.tearDownAdminData();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void uster() {
+
+	}
+
+	@Test(priority = 1)
+	public void SellerLogin() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		WebElement loginButton = driver.findElement(By.xpath("//input[@type='submit' and @value='Login']"));
+		Thread.sleep(2000);
+		loginButton.click();
+
+		AssertJUnit.assertTrue(
+		driver.getCurrentUrl().contains("http://localhost/cookingwithlove/seller/SellerMainPage.php"));
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+
+	@Test(priority = 2)
+	public void SellerAddDish() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Add New Dish")).click();
+		driver.findElement(By.name("dis_name")).sendKeys("TIRAMISU");
+		driver.findElement(By.name("dish_detailes")).sendKeys("TIRAMISU");
+		driver.findElement(By.name("food_price")).sendKeys("5");
+		WebElement dropdown = driver.findElement(By.id("dis_catagory"));
+		Select select = new Select(dropdown);
+		select.selectByVisibleText("Desserts & Sweets");
+		WebElement dropdown2 = driver.findElement(By.id("food_status"));
+		Select select2 = new Select(dropdown2);
+		select2.selectByVisibleText("Available");
+		WebElement fileInput = driver.findElement(By.id("Upload_pic"));
+		String projectDir = System.getProperty("user.dir");
+		String imagePath = projectDir + "\\src\\test\\resources\\TIRAMISU.jpg";
+		fileInput.sendKeys(imagePath);
+
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Add Dish']")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		String alertMessage = alert.getText();
+		Thread.sleep(2000);
+		alert.accept();
+
+		AssertJUnit.assertTrue(alertMessage.contains("Dish has been added successfully."));
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+	
+	@Test(priority = 3)
+	public void SellerUpdateDish() throws InterruptedException {
+	    driver.get("http://localhost/cookingwithlove/login.php");
+	    driver.findElement(By.id("username")).sendKeys("samarkal");
+	    driver.findElement(By.id("password")).sendKeys("Project123!");
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.linkText("Current Dishes")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.className("card-link")).click();
+	    driver.findElement(By.name("dis_name")).clear();
+	    driver.findElement(By.name("dis_name")).sendKeys("Cheesecake");
+	    driver.findElement(By.name("dish_detailes")).clear();
+	    driver.findElement(By.name("dish_detailes")).sendKeys("Blueberry Cheesecake");
+	    driver.findElement(By.name("food_price")).clear();
+	    driver.findElement(By.name("food_price")).sendKeys("4");
+		String projectDir = System.getProperty("user.dir");
+		String imagePath = projectDir + "\\src\\test\\resources\\cheesecake.jpg";
+	    driver.findElement(By.id("Upload_pic")).sendKeys(imagePath);
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//input[@type='submit' and @value='Update Dish']")).click();
+
+	    Alert alert = driver.switchTo().alert();
+	    String alertMessage = alert.getText();
+	    Thread.sleep(2000);
+	    alert.accept();
+
+	    AssertJUnit.assertTrue(alertMessage.contains("Dish has been Updated."));
+
+	    Thread.sleep(2000);
+	    driver.findElement(By.linkText("Log out")).click();
+	}
+
+	@Test(priority = 4)
+	public void SellerDeleteDish() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Current Dishes")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("card-link")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Delete Dish']")).click();
+
+		Alert alert = driver.switchTo().alert();
+		String alertMessage = alert.getText();
+		Thread.sleep(2000);
+		alert.accept();
+
+		AssertJUnit.assertTrue(alertMessage.contains("Dish has been Deleted."));
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+
+	@Test(priority = 10)
+	public void SellerEmptyDishesMessage() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Current Dishes")).click();
+		List<WebElement> cardLinkElements = driver.findElements(By.className("card-link"));
+
+	   while (!cardLinkElements.isEmpty()) {
+		    WebElement cardLink = cardLinkElements.get(0); 
+		    Thread.sleep(2000);
+		    cardLink.click();
+		    Thread.sleep(2000);
+		    driver.findElement(By.xpath("//input[@type='submit' and @value='Delete Dish']")).click();
+
+		    Alert alert = driver.switchTo().alert();
+		    Thread.sleep(2000);
+		    alert.accept();
+
+		    cardLinkElements = driver.findElements(By.className("card-link"));    
+		}
+
+		AssertJUnit.assertEquals(driver.findElement(By.xpath("//p[text()='No dishes found.']")).getText(), "No dishes found.");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+
+	@Test(priority = 5)
+	public void SellerCompleteActiveOrder() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Active Orders")).click();
+		
+		WebElement orderContainer = driver.findElement(By.className("order-container"));
+		List<WebElement> orderRows = orderContainer.findElements(By.tagName("tr"));
+		
+		while (!orderRows.isEmpty()) {
+	        Thread.sleep(2000);
+			WebElement row = orderRows.get(0);
+			row.findElement(By.xpath("//tr[1]/td[2]")).click();
+	        Thread.sleep(2000);
+	        driver.findElement(By.xpath("//input[@type='submit' and @value='Mark as Complete']")).click();
+		    Thread.sleep(2000);
+		   
+		    Alert alert = driver.switchTo().alert();
+		    Thread.sleep(2000);
+			String alertMessage = alert.getText();
+		    alert.accept();
+		    
+			AssertJUnit.assertTrue(alertMessage.contains("Order has been Marked as Completed successfully."));
+			
+		    orderContainer = driver.findElement(By.className("order-container"));
+		    orderRows = orderContainer.findElements(By.tagName("tr"));
+		}
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+	 
+	
+	@Test(priority = 6)
+	public void SellerEmptyActiveOrders() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Active Orders")).click();
+
+		AssertJUnit.assertEquals(driver.findElement(By.xpath("//p[text()='No Orders found.']")).getText(), "No Orders found.");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+	
+	@Test(priority = 7)
+	public void SellerMarkOrderPickedUp() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Ready for Pickup")).click();
+		
+		WebElement orderContainer = driver.findElement(By.className("order-container"));
+		List<WebElement> orderRows = orderContainer.findElements(By.tagName("tr"));
+        Thread.sleep(2000);
+		WebElement row = orderRows.get(0); 
+		row.findElement(By.xpath("//tr[1]/td[2]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@type='submit' and @value='Mark as Picked Up']")).click();
+		
+	    Alert alert = driver.switchTo().alert();
+	    Thread.sleep(2000);
+		String alertMessage = alert.getText();
+	    alert.accept();
+	    
+		AssertJUnit.assertTrue(alertMessage.contains("Order has been Marked as Picked Up."));
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+	 
+	@Test(priority = 8)
+	public void SellerViewCompletedOrder() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Completed Orders")).click();
+		
+		WebElement orderContainer = driver.findElement(By.className("order-container"));
+		List<WebElement> orderRows = orderContainer.findElements(By.tagName("tr"));
+        Thread.sleep(2000);
+		WebElement row = orderRows.get(0); 
+		row.findElement(By.xpath("//tr[1]/td[2]")).click();
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+	
+	@Test(priority = 9)
+	public void SellerViewCancelledOrders() throws InterruptedException {
+		driver.get("http://localhost/cookingwithlove/login.php");
+		driver.findElement(By.id("username")).sendKeys("samarkal");
+		driver.findElement(By.id("password")).sendKeys("Project123!");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@type='submit' and @value='Login']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Cancelled Orders")).click();
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Log out")).click();
+	}
+
+
+}
