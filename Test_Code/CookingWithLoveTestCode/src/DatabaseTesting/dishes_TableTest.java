bed43338e97125ea23dbef3405c76c90ba4b31c7
megbry <146877461+megbry@users.noreply.github.com>
2024-04-15 19:53:44 -0400

    Update dishes_TableTest.java
    Adjusted port to match project
    Adjusted accountID to avoid overlap

--- Test_Code/CookingWithLoveTestCode/src/DatabaseTesting/dishes_TableTest.java
+++ Test_Code/CookingWithLoveTestCode/src/DatabaseTesting/dishes_TableTest.java
@@ -15,18 +15,20 @@ public class dishes_TableTest {
 	@BeforeClass
 	public void setUp() throws SQLException {
 		String host = "localhost";
-	    Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + "/cooking_project", "root", "");
+		String port = "3306";
+	    Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port +  "/cooking_project", "root", "");
 	    Statement s = con.createStatement();
 	    
 	    // create user account for inserting dish to
 	    int rowsAffected = s.executeUpdate("INSERT INTO user_account (accountID, userName, password, accountType, firstName, lastName, email, phone, street, zip, DOB, accountRequestedDate, accountActiveDate, is_active) " +
-                "VALUES ('1', 'Testacc', 'Test1!', 'Seller', 'Test', 'Testing', 'test@gmail.com', '123-456-7890', '123 Test Road', '45678', '1901-01-01', NOW(), NULL, 'Y')");
+                "VALUES ('1234', 'Testacc', 'Test1!', 'Seller', 'Test', 'Testing', 'test@gmail.com', '123-456-7890', '123 Test Road', '45678', '1901-01-01', NOW(), NULL, 'Y')");
 	}
 	
 	@AfterClass
 	public void tearDown() throws SQLException {
 		String host = "localhost";
-	    Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + "/cooking_project", "root", "");
+		String port = "3306";
+	    Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port +  "/cooking_project", "root", "");
 	    Statement s = con.createStatement();
 		String usernameToDelete = "Testacc";
 		
@@ -38,11 +40,12 @@ public class dishes_TableTest {
 	 @Test
 	 public void testAddDish() throws SQLException {
 		 String host = "localhost";
-	     Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + "/cooking_project", "root", "");
+		 String port = "3306";
+	     Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port +  "/cooking_project", "root", "");
 	     Statement s = con.createStatement();
 	     // Execute the add statement
 	     int rowsAffected = s.executeUpdate("INSERT INTO dishes (dishID, dishName, description, price, status, dishType, accountID, publishDate, photoPath, is_published, DishRequestedDate)" + 
-	    		 "VALUES (NULL, 'Energy Drink', 'Gottagofast', '2.50', 'available', 'Soups', '1', NULL, NULL, 'Y', '2024-04-09')");
+	    		 "VALUES (NULL, 'Energy Drink', 'Gottagofast', '2.50', 'available', 'Soups', '1234', NULL, NULL, 'Y', '2024-04-09')");
 
 	     // Check if insertion was successful
 	     Assert.assertEquals(rowsAffected, 1, "Add dish failed.");
@@ -53,8 +56,9 @@ public class dishes_TableTest {
 	 @Test
 	 public void testDeleteDish() throws SQLException {
 		 String host = "localhost";
+		 String port = "3306";
 		 String dishNameToDelete = "Energy Drink";
-	     Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + "/cooking_project", "root", "");
+	     Connection con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port +  "/cooking_project", "root", "");
 	     Statement s = con.createStatement();
 
 	     // Execute the delete statement
